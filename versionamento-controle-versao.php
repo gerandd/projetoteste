dd2d579e756f22bf3644135b569141eb4e96a96f
Windows 10 <Windows 10@DESKTOP-I71RD25>
2017-04-15 13:13:31 -0300

    Este é meu segundo commit

---
+++ versionamento-controle-versao.php
@@ -0,0 +1,10 @@
+<?php
+/* é um local que vai guardar uma versao de seu codigo! 
+*Seria por exemplo um serviço de versionamento como o 
+* github (repositório) e outros 
+* O ato de enviar o código para um repositório de versionamento se chama "commit"
+* Vou comitar meu código -  significa que ela vai enviar seu codigo para o reporitório de versinamento
+*/
+
+?>
+
